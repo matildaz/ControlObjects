d354a37b969364b60c67f3dc2449ba7a007e222f
matildaz <irandom.con@mail.ru>
2021-08-18 14:07:53 +0300

    Remove button and complete binary switches

--- ControlObjects/ViewController.swift
+++ ControlObjects/ViewController.swift
@@ -9,27 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var button: UIButton!
     @IBOutlet var switches: [UISwitch]!
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
+    var number = 128 {
+        didSet {
+            number = (number + 256) % 256
+            updateUI()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rottateSwitches()
+        updateUI()
+        view.isUserInteractionEnabled = true
+        view.backgroundColor = .systemOrange
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func buttonGetPressed() {
+    func rottateSwitches() {
+        for item2 in switches {
+            item2.layer.transform = CATransform3DMakeRotation(-.pi/2, 0, 0, 1)
+        }
+    }
+    
+    func updateNumberFromTheSwitches() {
+        var count = 0
+        for item2 in switches {
+            count += item2.isOn ? item2.tag : 0
+        }
+        self.number = count
+    }
+    
+    func switchUpdate() {
+        for item in switches {
+            item.isOn = Int(number) & item.tag != 0
+        }
+    }
+    
+    /// option + cmd + / = Description
+    func updateUI() {
+        // присваваем значение для любых ситуаций []
+        textField.text = "\(number)"
+        switchUpdate()
+        slider.value = Float.init(number)
     }
     
     @IBAction func switchChange(_ sender: UISwitch) {
+        updateNumberFromTheSwitches()
     }
     
     @IBAction func sliderValueChange() {
+        number = Int(slider.value)
+        print(#line,#function)
     }
     
     @IBAction func textFieldGetChange() {
+        number = Int(textField.text ?? "") ?? 128
     }
 }
 
